74f7c271deaa7b1af297ca014f28d1850de327cb
Your Name <matheusbrantgo@gmail.com>
2020-10-26 20:56:32 -0300

    Merge branch 'main' of https://github.com/MatheusBrant/Algoritmo-genetico into main

--- obtem_pai.m
+++ obtem_pai.m
@@ -3,6 +3,22 @@ function [pai] = obtem_pai(populacao, vetor_adaptabilidade)
     % ao invés de obter um índice aleatório
     indice = randi([1 10]);
     pai = [populacao(indice, 1) populacao(indice, 2)];
+
+    %roleta
+    somatorio = 0;
+    sorteio = rand(1);
+
+    for i = 1 : 2,
+            for j=1:tamanho_populacao,
+                for k=1:j,
+                    somatorio = somatorio + populacao(k,3);
+                end
+                if sorteio <= somatorio,
+                    pai(i) = populacao(j,1);
+                    break;
+                end
+            end
+    end 
 end
 
 
